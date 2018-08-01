08e0b4e6f7c9e17d520c7b65f74bec6d49fa28d4
Chase Lee <clee231@uic.edu>
2018-08-01 04:23:56 +0000

    Adding cookbooks.

---
+++ cookbooks/chase1/README.md
@@ -0,0 +1,4 @@
+# chase
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/chase1/recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook:: chase
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+package 'nginx' do
+  action :install
+end
