8c43abd7d81e5e58cbda1e2b86da7a33135d79d8
Chase 李 <chase@emiya.io>
2021-03-13 02:24:02 -0600

    Updating bastion to not use a role assignment. Updating output variables.

---
+++ 2021-03-13/bastion-terraform/outputs.tf
@@ -0,0 +1,13 @@
+output "tls_private_key" {
+  value = tls_private_key.terraform_demo_ssh_key.private_key_pem
+  sensitive = true
+}
+
+output "bastion_ip" {
+  value = azurerm_public_ip.terraform_demo_public_ip.ip_address
+}
+
+output "bastion_fqdn" {
+  value = azurerm_dns_a_record.terraform_demo_dns_record.fqdn
+
+}
---
+++ 2021-03-13/bastion-terraform/provider.tf
@@ -0,0 +1,16 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.46.0"
+    }
+  }
+}
+
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x.
+    # If you're using version 1.x, the "features" block is not allowed.
+    features {}
+}
+
