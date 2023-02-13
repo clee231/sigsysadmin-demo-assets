29138972443358ebd8ea785f4102af16c2a39dd7
Chase 李 <clee231@uic.edu>
2023-02-12 22:28:13 -0600

    Update setup.sh

--- 2023-02-13/setup.sh
+++ 2023-02-13/setup.sh
@@ -11,8 +11,5 @@ do
     done
 
     apt-get update
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    apt install terraform
     apt install ansible
     apt install python3
